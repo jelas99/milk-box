21dc19d3f4eb12f8e5e1684e0f28f3c2282a2d0c
Joel Costa <joel.costa@natixis.com>
2021-01-26 16:35:17 +0000

    Main to test values

--- src/com/milk/MilkCalculator.java
+++ src/com/milk/MilkCalculator.java
@@ -5,13 +5,13 @@ public final class MilkCalculator {
 	/**
 	 * Number of boxes possible with some money and possible return of empty boxes
 	 * 
-	 * @param money allowed
-	 * @param price by box
+	 * @param money   allowed
+	 * @param price   by box
 	 * @param boxFree number of empty boxes to have free one
 	 * @return
 	 */
 	public int numberOfBoxs(int money, int price, int boxFree) {
-		
+
 		if (money < price) {
 			throw new RuntimeException("No Money");
 		}
@@ -26,43 +26,24 @@ public final class MilkCalculator {
 		if (possibleBoxs < free) {
 			return 0;
 		}
-		
+
 		if (possibleBoxs == free) {
 			return 1;
 		}
 
 		int newFreeBoxs = possibleBoxs / free;
 		int newIteration = newFreeBoxs + (possibleBoxs % free);
-		
+
 		return calculateNumberOfFreeBoxs(newIteration, free) + newFreeBoxs;
 	}
-	
-	
-	
-	
-//	private int calculateNumberOfFreeBoxsCycle(int possibleBoxs, int free) {
-//		int newFreeBoxs = 0;
-//		int newIteration = possibleBoxs;
-//		
-//		for (int i = 0; i < newIteration; i++) {
-//			if (newIteration < free) {
-//				break;
-//			}
-//			if (newIteration == free) {
-//				newFreeBoxs++;
-//				break;
-//			}
-//			
-//			
-//			newFreeBoxs = newFreeBoxs + newIteration / free;
-//			newIteration = newIteration / free + (newIteration % free);
-//			
-//			i = 0;
-//			
-//		}
-//		
-//
-//
-//		return newFreeBoxs;
-//	}
+
+	public static void main(String[] args) {
+		MilkCalculator calculator = new MilkCalculator();
+		int value = calculator.numberOfBoxs(15, 3, 2);
+		if (value != 9)
+			throw new RuntimeException("Bad Algorithm");
+		value = calculator.numberOfBoxs(18, 3, 2);
+		if (value != 11)
+			throw new RuntimeException("Bad Algorithm");
+	}
 }
